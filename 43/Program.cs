ddbf30e77f4c1c8213726546d20644b99f8ce79d
Jacksonville <e-evgeniy@yandex.ru>
2023-02-26 23:12:58 +0300

    sem_8 ds d q

--- 43/Program.cs
+++ 43/Program.cs
@@ -2,7 +2,6 @@
 //  двух прямых
 // // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
 //  значения b1, k1, b2 и k2 задаются пользователем
-Console.Clear();
 double pointX;
 double pointY;
 
